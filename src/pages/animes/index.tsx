0d72a8789030db264fa20e01f07097258222fc22
lyandeveloper <tutors3d@gmail.com>
2021-12-20 20:49:29 -0300

    feat: Add more data o db.json

--- src/pages/animes/index.tsx
+++ src/pages/animes/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { Header } from '../../components/Header';
 import { Autoplay, Pagination, Scrollbar, A11y } from 'swiper';
@@ -8,7 +9,6 @@ import styles from './styles.module.scss';
 import { Slide } from '../../components/Slide';
 import { EpisodeCard } from '../../components/EpisodeCard';
 import { AnimeCard } from '../../components/AnimeCard';
-import { useEffect, useState } from 'react';
 import api from '../../services/api'; 
 import { AnimeTypes } from '../../protocols/AnimeTypes';
 
@@ -102,30 +102,129 @@ export default function Animes() {
           <section className={`${styles.episodes_container} container`}>
             <h1>Novos Episódios</h1>
             <section className={styles.episodes_grid}>
-              <EpisodeCard 
-                thumbnail="https://www.animeunited.com.br/oomtumtu/2021/07/portada_jujutsu-kaisen-71.jpg" 
-                title="Jujutsu Kaisen" 
-                episode="01" 
-                createdDate="1min"
-              />
+                <EpisodeCard  
+                  thumbnail="https://www.animeunited.com.br/oomtumtu/2021/07/portada_jujutsu-kaisen-71.jpg" 
+                  title="Jujutsu Kaisen" 
+                  episode="01" 
+                  createdDate="1min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://animesrubro.net/wp-content/uploads/2021/10/Mushoku-Tensei-Isekai-Ittara-Honki-Dasu-2-01-Online-_-Animesrubro.jpg" 
+                  title="Mushoku Tensei: Isekai Ittara Honki" 
+                  episode="06" 
+                  createdDate="5min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABXp67rXI6DSqmbWIqv4_nLUAqhskGvkeRYc4B1254AMEx2b9qk4XqNq6cPzwSl-lEetL6CojjMAjlMpPij1wUhC8vRkD.jpg?r=78c" 
+                  title="Black Lagoon" 
+                  episode="04" 
+                  createdDate="10min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://capitulo.news/wp-content/uploads/2021/07/Seirei-Gensouki-Spirit-Chronicles-1.jpg" 
+                  title="Seirei Gensouki" 
+                  episode="04" 
+                  createdDate="20min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://hacchifansub.net/wp-content/uploads/2018/06/kuinaki-sentaku.jpg" 
+                  title="Shingeki no Kyojin: Kuinaki Sentaku" 
+                  episode="18" 
+                  createdDate="25min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="http://chuvadenanquim.files.wordpress.com/2012/05/bakemono5.jpg" 
+                  title="Bakemonogatari" 
+                  episode="06" 
+                  createdDate="45min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://www.animedia.com.br/wp-content/uploads/2017/04/accel.jpg" 
+                  title="Accel World" 
+                  episode="08" 
+                  createdDate="48min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://1.bp.blogspot.com/-dYwEM61x1_s/XWCciJwdN2I/AAAAAAAAEkw/EYjIglx2iJk8tBfULWpjeBewaokqvzHWgCLcBGAs/s1600/arslan-senki.jpg" 
+                  title="Arslan Senki" 
+                  episode="15" 
+                  createdDate="56min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://sucodemanga.com.br/wp-content/uploads/2021/07/naruto-shippuden-funimation-thumb.jpg" 
+                  title="Naruto Shippuuden" 
+                  episode="128" 
+                  createdDate="1h"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://occ-0-1722-1723.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbOVv3bexNpP3CGl0s4nyP2tOyRql69nWgnxxnORDZeqIkPipZzUxg_12ppwhS9daXl0c5uZbsUKqoeEaql-kGvRrLYi.jpg?r=7ce" 
+                  title="Fairy Tail" 
+                  episode="300" 
+                  createdDate="1h15min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://criticalhits.com.br/wp-content/uploads/2021/06/Goku-vegeta.jpg" 
+                  title="Dragon Ball Super" 
+                  episode="80" 
+                  createdDate="1h30min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://www.myanimesonline.biz/wp-content/uploads/2019/01/hunter-x-hunter-2011-episodio-64.jpg" 
+                  title="Hunter x Hunter (2011)" 
+                  episode="20" 
+                  createdDate="1h45min"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://ptanime.com/wp-content/uploads/2020/04/Boku-no-Hero-Academia-4-cover.jpg" 
+                  title="Boku No Hero Academia" 
+                  episode="12" 
+                  createdDate="2h"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://i0.wp.com/www.jbox.com.br/wp/wp-content/uploads/2021/12/ichigo-bleach-trailer-destacada.jpg?fit=774%2C489&quality=99&strip=all&ssl=1" 
+                  title="Bleach" 
+                  episode="120" 
+                  createdDate="1 dia"
+                  href='#'
+                />
+                <EpisodeCard  
+                  thumbnail="https://rollingstone.uol.com.br/media/_versions/one-piece-live-action-series-seemingly-coming-to-netflix_1fcz.h720_widelg.jpg" 
+                  title="One Piece" 
+                  episode="500" 
+                  createdDate="2 dias"
+                  href='#'
+                />
             </section>
           </section>
 
           <section className={`${styles.episodes_container} container`}>
             <h1>Últimos lançados</h1>
             <section className={styles.episodes_grid}>
-              {animes.map((anime : AnimeTypes) => (
-                <AnimeCard
-                  key={anime.id} 
-                  link={`/animes/${anime.slug}`} 
-                  thumbnail={anime.thumbnail}
-                  dub={anime.dub}
-                  episodes={anime.episodes_amount}
-                  title={anime.title} 
-                  type={anime.type}
-                  status={anime.status}
-                />
-              ))}
+              {animes.map((anime : AnimeTypes) => ( 
+                    <AnimeCard
+                      key={anime.id} 
+                      link={`/animes/${anime.slug}`} 
+                      thumbnail={anime.thumbnail}
+                      dub={anime.dub}
+                      episodes={anime.episodes_amount}
+                      title={anime.title} 
+                      type={anime.type}
+                      status={anime.status}
+                    /> 
+                ))}
             </section>
           </section>
         </div>
